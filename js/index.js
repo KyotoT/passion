927209c3dbbe158d7995e6d3a1f138c64c193034
KyotoT <57787316+KyotoT@users.noreply.github.com>
2020-11-21 13:10:57 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -35,7 +35,13 @@ $(function(){
   });
 });
 
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
 
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
 
 $(function () {
   $(window).on('scroll', function () {
@@ -44,7 +50,7 @@ $(function () {
       // $('#mainContents span').text(scrollpx);
       if (scrollpx > 4000) {
           mainContents.css({
-              background: 'url(../img/bg-img/bg-img4.jpg) repeat',
+              background: 'url(https://i.pinimg.com/originals/4c/b3/19/4cb3193989a9f649125958061913cdbb.jpg) repeat',
           });
       } else if (scrollpx > 3000) {
           mainContents.css({
@@ -52,15 +58,15 @@ $(function () {
           });
       } else if (scrollpx > 2000) {
           mainContents.css({
-              background: 'url(../img/bg-img/bg-img3.jpg) repeat'
+              background: 'url(https://i.pinimg.com/originals/b3/36/d3/b336d3c1f85a09ca51a5e55b60de74a4.jpg) repeat'
           });
       } else if (scrollpx > 1000) {
           mainContents.css({
-              background: 'url(../img/bg-img/bg-img2.jpg) repeat-x center left'
+              background: 'url(https://i.pinimg.com/originals/7e/5e/eb/7e5eeb6a4f9e0d89749d8b2e240134a2.jpg) repeat-x center left'
           });
       } else {
           mainContents.css({
-              background: 'url(../img/bg-img/bg-img01.jpg) repeat-x'
+              background: 'url(https://i.pinimg.com/originals/74/22/47/7422470c65502f368f9f823889234eae.jpg) repeat-x'
 
           });
       }
